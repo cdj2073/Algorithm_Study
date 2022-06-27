b8f1ea57ea360263b539148f703155e6859deeff
cdj2073 <ab8922@naver.com>
2022-06-28 08:12:02 +0900

    create ex02.py

---
+++ python_for_coding_test/1_greedy/ex02.py
@@ -0,0 +1,34 @@
+# N, M, K를 공백으로 구분하여 입력받기
+n, m, k = map(int, input().split())
+# N개의 수를 공백으로 구분하여 입력받기
+data = list(map(int, input().split()))
+
+data.sort() # 입력받은 수 정렬
+first = data[n - 1]
+second = data[n - 2]
+
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result = 0
+result += count * first
+result += (m - count) * second
+
+print(result)
+'''
+# 단순하게 푸는 방법 => M의 크기가 100억 이상처럼 커진다면 time out
+result = 0
+
+while True:
+    for i in range(k):
+        if m == 0:
+            break
+        result += first
+        m -= 1
+    if m == 0:
+        break
+    result += second
+    m -= 1
+
+print(result)
+'''
